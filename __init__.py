165788d534e879d74ccd42789c2337d60e0c6ab1
slyt <you@example.com>
2024-06-23 19:33:04 -0400

    Add List To String node

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .pull_model import PullModel
 from .download_huggingface_models import DownloadHuggingfaceModel
-from .list_models import ListModels
+from .list_models import ListModels, ListToString
 from .generate import GenerateOllama
 
 # A dictionary that contains all nodes you want to export with their names
@@ -9,15 +9,18 @@ NODE_CLASS_MAPPINGS = {
     "PullModel": PullModel,
     "ListModels": ListModels,
     "DownloadHuggingfaceModel": DownloadHuggingfaceModel,
-    "GenerateOllama": GenerateOllama
+    "GenerateOllama": GenerateOllama,
+    "ListToString": ListToString
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PullModel":"Pull Model | Ollama Nodes",
     "ListModels":"List Models | Ollama Nodes",
+    "ListToString":"List to String | Ollama Nodes",
     "DownloadHuggingfaceModel":"Download Huggingface Model | Ollama Nodes",
     "GenerateOllama":"Generate Text | Ollama Nodes"
+    
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
--- list_models.py
+++ list_models.py
@@ -2,7 +2,6 @@ import ollama
 from .logging_setup import setup_logging
 import logging
 
-
 class ListModels:
     def __init__(self):
         setup_logging()
@@ -12,7 +11,7 @@ class ListModels:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "delimiter": ("STRING", {"default": "\\n"}),
+                "delimiter": ("STRING", {"default": ","}),
             }
         }
 
@@ -21,8 +20,8 @@ class ListModels:
         return float("NaN") # Always execute the node.
 
 
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("model_list",)
+    RETURN_TYPES = ("LIST","STRING")
+    RETURN_NAMES = ("model_list","model_list_as_string")
 
     FUNCTION = "list_models"
 
@@ -41,6 +40,41 @@ class ListModels:
         model_name_list = []
         for model in response['models']:
             model_name_list.append(model["name"])
-        model_list_newline_delimited = delimiter.join(model_name_list)
-        return (model_list_newline_delimited,)
+        model_list_delimited = delimiter.join(model_name_list)
+        return (model_name_list, model_list_delimited,)
 
+# cannot use StringListToString becuase class names must be globally unique
+# and StringListToString is already used in ComfyUI-Impact-Pack
+class ListToString: 
+    def __init__(self):
+        setup_logging()
+        self.logger = logging.getLogger("comfyui-ollama-nodes.ListModels")
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "list": ("LIST", {"forceInput": True}),
+            }
+        }
+
+    @classmethod
+    def IS_CHANGED(s):
+        return float("NaN") # Always execute the node.
+
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("string",)
+
+    FUNCTION = "list_to_string"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "LLM/Ollama"
+
+    def list_to_string(self, list):
+
+        output_string = ""
+        for element in list:
+            output_string += str(element) # cast so we can handle non-string inputs
+        return (output_string,)
